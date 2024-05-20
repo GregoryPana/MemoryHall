378d7dd1e7f1ed293efb424f71805efbcd3791b1
GregoryPana <107904435+GregoryPana@users.noreply.github.com>
2024-05-20 15:47:51 +0800

    console display images

--- show_all.js
+++ show_all.js
@@ -1,10 +1,12 @@
 let res = document.getElementById('gallery');
 
 // Function to display all imaes regardless of category
+x = 0
 
 function diplayImages() {
-
+    x = x+1;
     let displayItems = data.map(function (item) {
+        console.log(x);
 
         // Returns the image url and the category
         return `<img src=${item.path} alt=${item.type} />`;
